5a1a1d07848333ae73f923c6a6a17c94aa4f9f3d
robert wickel <robert.wickel@gmx.de>
2026-02-12 20:13:50 +0100

    Cleanup root files, fix app README typo, and push latest state

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.yourworkouttimer.app',
-  appName: 'Your Workout Timer',
-  webDir: 'dist',
-};
-
-export default config;
